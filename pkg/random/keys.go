095a58c93d7cb620acc59cce7393c22f9339ffee
Boban Acimovic <boban.acimovic@gmail.com>
2021-07-20 19:08:18 +0200

    ingore gomnd lint warnings

--- pkg/random/keys.go
+++ pkg/random/keys.go
@@ -11,12 +11,12 @@ const (
 
 // GenerateKeys generates AWS compatible access and secret access keys.
 func GenerateKeys() (accessKey string, secretAccessKey string, err error) {
-	ak, err := Random(accessKeyChars, 20)
+	ak, err := Random(accessKeyChars, 20) //nolint:gomnd
 	if err != nil {
 		return "", "", fmt.Errorf("random access key: %w", err)
 	}
 
-	sak, err := Random(secretAccessKeyChars, 40)
+	sak, err := Random(secretAccessKeyChars, 40) //nolint:gomnd
 	if err != nil {
 		return "", "", fmt.Errorf("random secret access key: %w", err)
 	}
